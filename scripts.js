3354c36eab74a32941ded56505ab327c57986a03
Deus <amadeusrm97@gmail.com>
2022-09-27 09:26:11 -0400

    added miliseconds hand

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
-setInterval(setClock, 1000)
+setInterval(setClock, 0)
 
 const hourHand = document.querySelector('[data-hour-hand]')
-const minuteHand = document.querySelector('[data-minute-hand]')
-const secondHand = document.querySelector('[data-second-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]') 
+const secondHand = document.querySelector('[data-second-hand]') 
+const miliSecondHand = document.querySelector('[data-miliSecond-hand]') 
 
 function setClock(){
     const currentDate = new Date()
-    const secondsRatio = currentDate.getSeconds() / 60
+    const miliSecondRatio = currentDate.getMilliseconds() / 1000
+    const secondsRatio = (miliSecondRatio + currentDate.getSeconds()) / 60
     const minutesRatio = (secondsRatio + currentDate.getMinutes())  / 60
     const hoursRatio = (minutesRatio + currentDate.getHours())  / 12
 
+    setRotation(miliSecondHand, miliSecondRatio)
     setRotation(secondHand, secondsRatio)
     setRotation(minuteHand, minutesRatio)
     setRotation(hourHand, hoursRatio)
